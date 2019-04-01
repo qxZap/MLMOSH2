2d88f3e9a338c522f45cbbe3a80ebbe54c5c2c20
Milea Mihai-Cristian <MileaMihai@outlook.it>
2019-04-01 12:10:10 +0300

    Add files via upload

---
+++ gameselector.py
@@ -0,0 +1,11 @@
+import os
+#print("Select game:")
+runner = {
+	"1":"python3 zork.py",
+	"2":"python /usr/local/bin/pywumpus.py"
+}
+while True:
+	print("1. Zork\n2. Wumpus\n")
+	input_text = input("Choose game:")
+	os.system(runner[input_text])
+	break
