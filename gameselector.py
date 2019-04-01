eec8fae369b6f8c7dd53c760f4af65691ada23e7
Milea Mihai-Cristian <MileaMihai@outlook.it>
2019-04-01 13:03:18 +0300

    Update gameselector.py

--- gameselector.py
+++ gameselector.py
@@ -1,7 +1,7 @@
 import os
 #print("Select game:")
 runner = {
-	"1":"/usr/bin/python3 /opt/app/MLMOS2/zork.py",
+	"1":"python3 /usr/local/bin/zork.py",
 	"2":"python /usr/local/bin/pywumpus.py"
 }
 while True:
