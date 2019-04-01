afc73d457c93a3fd541b6600afd334043700e630
Milea Mihai-Cristian <MileaMihai@outlook.it>
2019-04-01 13:24:18 +0300

    Add files via upload

--- gameselector.py
+++ gameselector.py
@@ -3,9 +3,10 @@ import os
 runner = {
 	"1":"python3 zork.py",
 	"2":"python /usr/local/bin/pywumpus.py"
+	"2":"python3 adventureontheweb.py"
 }
 while True:
-	print("1. Zork\n2. Wumpus\n")
+	print("1. Zork\n2. Wumpus\n3. Adventure On The Web\n")
 	input_text = input("Choose game:")
 	os.system(runner[input_text])
 	break
