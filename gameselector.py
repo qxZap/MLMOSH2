aeaa298b3d25d56f1d3c87902dfac74b4d9a77ea
Milea Mihai-Cristian <MileaMihai@outlook.it>
2019-04-01 14:13:25 +0300

    Update gameselector.py

--- gameselector.py
+++ gameselector.py
@@ -3,7 +3,7 @@ import os
 runner = {
 	"1":"python3 /opt/app/MLMOSH2/zork.py",
 	"2":"python /usr/local/bin/pywumpus.py",
-	"3":"python3 /opt/app/MLMOSH2/adventureontheweb.py"
+	"3":"python /opt/app/MLMOSH2/adventureontheweb.py"
 }
 while True:
 	print("1. Zork\n2. Wumpus\n3. Adventure On The Web\n")
